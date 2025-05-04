f1c6e262f1cdde24fedc45ced860c06659430f29
Engiavi <hackavi796@gmail.com>
2025-05-05 00:07:00 +0530

    importing library and check gpu is available or not

---
+++ tensor.py
@@ -0,0 +1,8 @@
+import torch
+
+if torch.cuda.is_available():
+    print("GPU is available!")
+    print(f"Using GPU: {torch.cuda.get_device_name(0)}")
+else:
+    print("GPU not available. Using CPU.")
+    
