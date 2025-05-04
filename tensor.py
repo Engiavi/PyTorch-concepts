833c67956160adf4bf501ec3e17aa653e9740730
Engiavi <hackavi796@gmail.com>
2025-05-05 00:10:32 +0530

    create tensor using empty method

--- tensor.py
+++ tensor.py
@@ -1,8 +1,16 @@
 import torch
 
-if torch.cuda.is_available():
-    print("GPU is available!")
-    print(f"Using GPU: {torch.cuda.get_device_name(0)}")
-else:
-    print("GPU not available. Using CPU.")
+# if torch.cuda.is_available():
+#     print("GPU is available!")
+#     print(f"Using GPU: {torch.cuda.get_device_name(0)}")
+# else:
+#     print("GPU not available. Using CPU.")
     
+# Create a tensor
+
+# using empty
+x = torch.empty(5, 3)
+print(x)
+
+# check the type of variable
+print(type(x))
