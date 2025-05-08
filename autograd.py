a2609a107436e48112f13b2d1b2116f2df24987d
Engiavi <hackavi796@gmail.com>
2025-05-08 22:24:54 +0530

    code for chain differentation of 3 tensors and apply backward propagation

--- autograd.py
+++ autograd.py
@@ -10,4 +10,15 @@ y = x ** 2
 
 # to calculate the derivative we have to call backward() function
 y.backward() # this will calculate the derivative of y with respect to x
-print("x.grad:",x.grad) # this will print the derivative of y with respect to x
+# print("x.grad:",x.grad) # this will print the derivative of y with respect to x
+
+#  now we are creating three tensor x, y and z
+x = torch.tensor(3.0, requires_grad=True)
+y = x ** 2
+z = torch.sin(y) # this will create a new tensor z which is the sine of y
+
+# print("x:",x,"\n","y:",y,"\n","z:",z) # print the value of x, y and z
+
+z.backward()
+
+print("derivative: ", x.grad)
